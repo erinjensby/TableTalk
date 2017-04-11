0d59435862e39b7560eca176e1eb470b4eb8c359
Erin Jensby <erinjensby@wireless-10-147-20-58.public.utexas.edu>
2017-04-10 19:12:39 -0500

    Implementing GoogleMaps API

---
+++ TableTalk/StudyLocations.swift
@@ -0,0 +1,17 @@
+//
+//  StudyLocations.swift
+//  TableTalk
+//
+//  Created by Erin Jensby on 4/10/17.
+//  Copyright © 2017 Nicolas Lavigne. All rights reserved.
+//
+
+import Foundation
+
+struct StudyLocations {
+    
+//    static let locationNames:[String] = ["Epoch Coffee", "Monkey Nest Coffee", "GDC"]
+    
+    static let locationIDs:[String] = ["ChIJG-gJw2vKRIYROWi2uwOp8QE", "ChIJ0zs2zVjKRIYRuP3X3KCjGOM", "ChIJcTd6n5y1RIYRwfBnY4RaTqU"]
+    
+}
